1eb48313598f33c13a8f3074ab8923bf38d57f8d
Jose Alvarez <alvarezloaiciga@gmail.com>
2022-01-24 17:19:17 -0600

    Root page

---
+++ spec/requests/home_page_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Home page", type: :request do
+
+  it "renders the home page" do
+    get "/"
+    expect(response).to render_template(:home)
+  end
+
+end
