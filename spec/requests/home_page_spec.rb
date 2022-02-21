70b59c4e287556487a2dbebf3b62b568dd465086
Jose Alvarez <alvarezloaiciga@gmail.com>
2022-02-21 16:59:02 -0600

    Fix tests

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -3,6 +3,11 @@
 require "rails_helper"
 
 RSpec.describe "Home page", type: :request do
+  let(:user) { User.create(email: "user@email.com", password: "password") }
+
+  before do
+    sign_in user
+  end
 
   it "renders the home page" do
     get "/"
